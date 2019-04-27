da16be706f5dacc2af227cdd0b035c356f38c9be
Sammy Hamdan <sammy.hamdan@gmail.com>
2019-04-26 20:14:27 -0700

    Helper methods, loading bar, error message, Async class.

--- app/src/main/java/com/hemingwaywest/utiliserve/FormFragment.java
+++ app/src/main/java/com/hemingwaywest/utiliserve/FormFragment.java
@@ -1,5 +1,6 @@
 package com.hemingwaywest.utiliserve;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -9,12 +10,15 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.hemingwaywest.utiliserve.Models.FormListItem;
 import com.hemingwaywest.utiliserve.Utilities.RecyclerViewAdapter;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -27,7 +31,9 @@ import java.util.List;
  * Date: 4/8/2019
  * url:  www.HemingwayWest.com
  * Last Modified: $file.lastModified
- * Notes:
+ * Notes: This class will setup the recycler view and connect the data to the adapter and then
+ * to the recycler view.
+ * Contains an inner class to run the Async Task of populating the view
  */
 public class FormFragment extends Fragment {
 
@@ -36,18 +42,40 @@ public class FormFragment extends Fragment {
     private List<FormListItem> mData;
     private JSONArray mJSONarray;
 
+    //Loading and Errors
+    private TextView mErrorMessageDisplay;
+    private ProgressBar mLoadingBar;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //return super.onCreateView(inflater, container, savedInstanceState);
         formView = inflater.inflate(R.layout.fragment_forms, container, false);
+
+        //Find all variables in xml
         myRecyclerView = (RecyclerView)formView.findViewById(R.id.form_recyclerView);
+        mErrorMessageDisplay = (TextView)formView.findViewById(R.id.tv_error_message_display);
+        mLoadingBar = (ProgressBar)formView.findViewById(R.id.pb_loading_indicator);
+
+        //Pass data to the adapter
         RecyclerViewAdapter recycleAdapter = new RecyclerViewAdapter(getContext(), mData);
         myRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         myRecyclerView.setAdapter(recycleAdapter);
         return null;
     }
 
+    //region PRIVATE HELPER METHODS
+    private void showFormViews(){
+        mErrorMessageDisplay.setVisibility(View.INVISIBLE);
+        myRecyclerView.setVisibility(View.VISIBLE);
+    }
+    private void showErrorMessage(){
+        myRecyclerView.setVisibility(View.INVISIBLE);
+        mErrorMessageDisplay.setVisibility(View.VISIBLE);
+    }
+    //endregion
+
+    //region JSON METHODS
     private String loadJSONFromAsset(){
         String json = null;
         try{
@@ -79,5 +107,52 @@ public class FormFragment extends Fragment {
         //TODO: Put JSON data into array format
         RecyclerViewAdapter rvAdapter = new RecyclerViewAdapter(formView.getContext(), mData);
         myRecyclerView.setAdapter(rvAdapter);
+    }
+
+    //endregion
+
+    /**
+     * Private inner class to start the background thread activity
+     * TODO Check to see if in debug mode or live mode from preferneces
+     */
+    public class FetchFormTask extends AsyncTask<String, Void, String[]> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoadingBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String[] doInBackground(String... params) {
+            //If there's no data pulled, dip out
+            if (params.length ==0){
+                return null;
+            }
+
+            //Get params in order from JSON helper, params[0] etc
+            String forms = params[0];
+
+            try{
+                //stuff
+            }catch (Exception e){
+                e.printStackTrace();
+                return null;
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String[] strings) {
+            super.onPostExecute(strings);
+            mLoadingBar.setVisibility(View.INVISIBLE);
+            if (mData != null){
+                showFormViews();
+            }
+            else{
+                showErrorMessage();
+            }
+        }
     }
 }
--- app/src/main/java/com/hemingwaywest/utiliserve/Utilities/RecyclerViewAdapter.java
+++ app/src/main/java/com/hemingwaywest/utiliserve/Utilities/RecyclerViewAdapter.java
@@ -3,17 +3,13 @@ package com.hemingwaywest.utiliserve.Utilities;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewParent;
 import android.widget.TextView;
 
 import com.hemingwaywest.utiliserve.Models.FormListItem;
 import com.hemingwaywest.utiliserve.R;
-
-import org.w3c.dom.Text;
 
 import java.util.List;
 
@@ -30,15 +26,23 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     //Require a view holder, a context, and data
     Context mContext;
-    List<FormListItem> mData;
+    List<FormListItem> mFormData;
 
     public RecyclerViewAdapter(Context mContext, List<FormListItem> mData) {
         this.mContext = mContext;
-        this.mData = mData;
+        this.mFormData = mData;
     }
 
+    /**
+     * This gets called when each new viewholder is created.
+     *
+     * @param viewGroup The viewgroup that these viewholders are contained within
+     * @param viewType if the recycler view has more than one type of item, you use this viewType Integer
+     *          to provide a different layout.
+     * @return A new Recycler view Adapter that holds the view for each list item
+     */
     @Override
-    public MyViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+    public MyViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
         View v;
         v = LayoutInflater.from(mContext).inflate(R.layout.item_form,viewGroup,false);
         MyViewHolder vHolder = new MyViewHolder(v);
@@ -50,15 +54,21 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     /**
      * Fetch Data from Model
      */
-    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
-        myViewHolder.tv_formName.setText(mData.get(i).getFormTitle());
-        myViewHolder.tv_formDetails.setText(mData.get(i).getFormDetails());
+    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int position) {
+        myViewHolder.tv_formName.setText(mFormData.get(position).getFormTitle());
+        myViewHolder.tv_formDetails.setText(mFormData.get(position).getFormDetails());
 
     }
 
     @Override
     public int getItemCount() {
-        return mData.size();
+        if (null == mFormData) return 0;
+        return mFormData.size();
+    }
+
+    public void setFormData (List<FormListItem> formData){
+        mFormData = formData;
+        notifyDataSetChanged();
     }
 
 
